2d11f60f218ad36b5c06ec0b36c3e2cbbf1f2ed7
akp2108@gmail.com <Brisbane@123>
2015-09-01 14:05:38 +1000

    No Changes Made in the First Build

---
+++ src/test/java/com/github/maths/Calculations.java
@@ -0,0 +1,27 @@
+package com.github.maths;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Calculations {
+	
+	private WebDriver driver;
+	
+  @Test
+  public void addition() {
+	  
+	  driver = new FirefoxDriver();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.manage().window().maximize();
+	  
+	  driver.get("http://docs.seleniumhq.org/");
+	  
+	  String sTitle = driver.getTitle();
+	  System.out.println(sTitle);  
+	  
+  }
+  
+}
